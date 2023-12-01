bd8d23a946aabb78d21e3731d87d60536fff7fab
Ishan Deshpande <deshpandeishan75@gmail.com>
2023-12-01 16:40:29 +0530

    Fixed the arrow bug.

--- main.py
+++ main.py
@@ -4,27 +4,24 @@ from random import randint
 screen = turtle.Screen()
 screen.setup(width=1000, height=1000)
 
-
 screen.bgcolor("#92B6F5")
 screen.title("Welcome to the turtle race!")
 
 kasav = turtle.Turtle()
 kasav.penup()
 kasav.hideturtle()
-kasav.speed(0)
 
 turtle.tracer(False)
-
+kasav.goto(-200, 50)
 square_size = 10
-
 for i in range(0, 30, square_size):
-    for j in range(0, 430, square_size):
+    for j in range(0, 320, square_size):
         if (i // square_size + j // square_size) % 2 == 0:
             kasav.fillcolor("white")
         else:
             kasav.fillcolor("black")
         kasav.penup()
-        kasav.goto(i + 250, j + -200)
+        kasav.goto(i + 230, j + -120)
         kasav.pendown()
         kasav.begin_fill()
         for _ in range(4):
@@ -32,22 +29,20 @@ for i in range(0, 30, square_size):
             kasav.right(90)
         kasav.end_fill()
 
+
 turtle.update()
 
-
 turtle.tracer(True)
-turtle.speed(10)
 screen.title("Welcome to turtle race!")
 color_set = (113, 231, 247)
 hex_color = "#{:02x}{:02x}{:02x}".format(*color_set)
 screen.bgcolor(hex_color)
 
-user_bet = screen.textinput(title="Enter your bet", prompt="Choose the color of the turtle that you think will win the race:" )
+user_bet = screen.textinput(title="Enter your bet", prompt="Choose the color of the turtle that you think will win the race:")
 colors_list = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
 y_coordinates = [-100, -50, 0, 50, 100, 150]
 
 turtles = []
-
 for _ in range(0, 6):
     new_turtle = turtle.Turtle(shape="turtle")
     new_turtle.color(colors_list[_])
